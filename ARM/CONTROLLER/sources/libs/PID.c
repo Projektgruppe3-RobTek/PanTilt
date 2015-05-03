f79ebcf88226914cee22c5b6f192ff860b3958e8
StefanRvo <stefan@stefanrvo.dk>
2015-05-03 12:09:14 +0200

    added missing comments.

--- ARM/CONTROLLER/sources/libs/PID.c
+++ ARM/CONTROLLER/sources/libs/PID.c
@@ -4,15 +4,16 @@ double PID(PID_s *PID_struct, double error)
   //Propotional gain
   double proportional_part = error * PID_struct->K_p;
 
+  //Integral gain
   PID_struct->integral += error;
+  double integral_part  = PID_struct->integral * PID_struct->K_i;
 
-  double integral_part = PID_struct->integral * PID_struct->K_i;
-
-  double derivative_part = PID_struct->K_d * (error - PID_struct->last_error);
-
+  //derivative gain.
+  double derivative_part   = PID_struct->K_d * (error - PID_struct->last_error);
   PID_struct -> last_error = error;
+
   return proportional_part + integral_part + derivative_part;
-  
+
 }
 
 PID_s init_PID(double K_p, double K_i, double K_d)
@@ -22,8 +23,8 @@ PID_s init_PID(double K_p, double K_i, double K_d)
   PID_struct.K_i = K_i;
   PID_struct.K_d = K_d;
 
-  PID_struct.integral = 0;
+  PID_struct.integral   = 0;
+  PID_struct.last_error = 0;
 
   return PID_struct;
-
 }
